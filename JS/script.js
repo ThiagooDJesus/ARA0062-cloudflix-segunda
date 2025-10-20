a79a3e93132009e95164a12d4c4f5643bd360005
ThiagooDJesus <thiagodjesuslemos@gmail.com>
2025-10-20 20:43:27 -0300

    correcao

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,4 @@
 
-const carro = {
-marca: "Dodge",
-ano: 1979,
-nome: "Charger"
-}; 
-paragrafo - document.getElementById("p1");
-conteudo_html = "";
-
-for ( const cada_propriedade in carro ) {
- conteudo_html = conteudo_html + cada_propriedade + " = " + carro[cada_propriedade] + "<br>" ;
-}
-p1.innerHTML = conteudo_html;
 
 // O RegEx para validar e-mails é crucial aqui.
 // Esta é uma expressão regular comum para verificar se a string contém:
